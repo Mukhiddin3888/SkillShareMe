e1dacb498ffe53cc30b5d0b4ef3179cf978cb93e
Mukhiddin3888 <sirojiddinovm720@mail.ru>
2021-01-29 02:15:17 +0500

    splash partially complete, need to check change color

---
+++ app/src/main/java/com/example/skillshare/splash/FavoriteGenresFragment.kt
@@ -0,0 +1,113 @@
+package com.example.skillshare.splash
+
+import android.graphics.Color.parseColor
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.skillshare.R
+import kotlinx.android.synthetic.main.fragment_favorite_genres.*
+import kotlinx.android.synthetic.main.fragment_favorite_genres.view.*
+
+class FavoriteGenresFragment : Fragment() {
+    var choosen:Boolean= false
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_favorite_genres, container, false)
+
+        view.btn_fantasy.setOnClickListener{
+            if (choosen == false){
+                btn_fantasy.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_fantasy.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_thriller.setOnClickListener{
+            if (choosen == false){
+                btn_thriller.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_thriller.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_romans.setOnClickListener{
+            if (choosen == false){
+                btn_romans.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_romans.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_science.setOnClickListener{
+            if (choosen == false){
+                btn_science.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_science.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_others.setOnClickListener{
+            if (choosen == false){
+                btn_others.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_others.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_action.setOnClickListener{
+            if (choosen == false){
+                btn_action.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_action.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_biography.setOnClickListener{
+            if (choosen == false){
+                btn_biography.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_biography.setTextColor(parseColor("#FFFFFF"))
+                choosen = false
+            }
+        }
+
+        view.btn_literature.setOnClickListener{
+            if (choosen == false){
+                btn_literature.setTextColor(parseColor("#F16F54"))
+
+                choosen = true
+            }else{
+                btn_literature.setTextColor(parseColor("#FFFFFF"))
+            choosen = false
+        }
+        }
+
+        return view
+    }
+
+
+}
